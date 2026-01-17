fca60d4ea0077930605c6e2ba6bcc48e05f33aaf
Obed Korankye <obedkorankye91@gmail.com>
2026-01-17 09:42:33 +0000

    updated

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -5,9 +5,9 @@ function Counter() {
   return (
     <div>
       <p>Current Counter: {count}</p>
-      <button onClick={() => setCount(count + 1)}>increment</button>
-      <button onClick={() => setCount(count - 1)}>decrement</button>
-      <button onClick={() => setCount(0)}>reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
