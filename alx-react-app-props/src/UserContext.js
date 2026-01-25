3a2f0d46e1cf2dda2e70046552f82c82d7ae77b7
Obed Korankye <obedkorankye91@gmail.com>
2026-01-25 01:26:16 +0000

    Reversioned the UserContext.js

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,10 @@
+import React, { createContext, useState } from "react";
+const UserContext = createContext(null);
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState({ name: "Obed", email: "obed@example.com" });
+  const value = { user, setUser };
+
+  return React.createElement(UserContext.Provider, { value: value }, children);
+}
+export default UserContext;
---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserDetails() {
+  const { user } = useContext(UserContext);
+  return (
+    <div>
+      <h4>User Details</h4>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
