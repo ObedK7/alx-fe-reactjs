c8c91e3d72d8cdf1dda20383cde497547f7a94c4
ObedK7 <obedkorankye91@gmail.com>
2026-02-23 00:18:33 +0000

    feat: recipe-sharing-platform homepage created

---
+++ recipe-sharing-platform/src/data/recipes.js
@@ -0,0 +1,41 @@
+const recipesData = [
+  {
+    id: 1,
+    title: "Italian Dish",
+    summary: "A classic Italian pasta dish...",
+    image: "/images/italian_dishes.jpg",
+  },
+  {
+    id: 2,
+    title: "Chicken Tikka Masala",
+    summary: "Chunks of grilled chicken...",
+    image: "/images/Tandoori-Chicken-20.jpg",
+  },
+  {
+    id: 3,
+    title: "Avocado Toast",
+    summary: "Toasted bread with avocado...",
+    image: "/images/toasted_bread.jpg",
+  },
+
+  {
+    id: 4,
+    title: "Italian Dish",
+    summary: "A classic Italian pasta dish...",
+    image: "/images/italian_dishes.jpg",
+  },
+  {
+    id: 5,
+    title: "Chicken Tikka Masala",
+    summary: "Chunks of grilled chicken...",
+    image: "/images/Tandoori-Chicken-20.jpg",
+  },
+  {
+    id: 6,
+    title: "Avocado Toast",
+    summary: "Toasted bread with avocado...",
+    image: "/images/toasted_bread.jpg",
+  },
+];
+
+export default recipesData;
